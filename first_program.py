abcd52938a2902aec1ed39c9f8d4704e32d079c4
Nisan96 <nayanassassinator94@gmail.com>
2022-08-04 00:08:44 +0600

    https://github.com/Nisan96/Python.git

---
+++ first_program.py
@@ -0,0 +1,7 @@
+fname = input("Enter first name:")
+midname = input("Enter middle name:")
+lname = input("Enter last name:")
+
+gmail = fname + '.' + midname + '.'+ lname + '@gmail.com'
+
+print("your Gmail is:",gmail)
