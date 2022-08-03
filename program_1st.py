34b8d8e646f0d52d00599f5fa040f3690202e392
Nisan96 <nayanassassinator94@gmail.com>
2022-08-04 01:46:33 +0600

    modify

--- program_1st.py
+++ program_1st.py
@@ -1,5 +1,5 @@
 fname = input("Enter 1st name:")
 lname = input("Enter last name:")
 dob = input("Enter date of birth:")
-gmail = fname + lname + "#" + dob + "@gmail.com"
+gmail = fname + lname + "_" + dob + "@gmail.com"
 print("your email is:",gmail)
